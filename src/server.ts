6e4ae9f328b2c047de0d5daffa599c8d62c727e6
WalterDesktop <contato.walterretke@gmail.com>
2024-06-25 23:08:58 -0300

    fix register fastify

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import Fastify from 'fastify';
 import { routes } from './routes'; // Verifique se o caminho está correto
+import fastifyCors from '@fastify/cors';
 
 const fastify = Fastify({
     logger: true
+});
+
+fastify.register(fastifyCors, {
+    origin: true // Permitir todas as origens
 });
 
 fastify.register(routes);
@@ -10,7 +15,7 @@ fastify.register(routes);
 const start = async () => {
     try {
         await fastify.listen({ port: 3100 });
-        console.log('Server listening on http://localhost:3000');
+        console.log('Server listening on http://localhost:3100');
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
